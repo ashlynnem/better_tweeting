05ced4bf1fbc5edb76ff8ca622f41833b65215c6
Ashlynne Morgan <Ashlynne@Ashlynnes-MacBook-Air.local>
2015-08-19 20:30:09 -0400

    First Commit

---
+++ app/controllers/epicenter_controller.rb
@@ -0,0 +1,49 @@
+class EpicenterController < ApplicationController
+  def feed
+  	# TTS-SLIDE28
+    # Create and initialize the array
+  	@following_tweets = []
+
+  	# Get all the tweets
+  	@tweets = Tweet.all
+
+  	# Sort and find the ones with the current user
+  	@tweets.each do |tweet|
+  		current_user.following.each do |f|
+  			if tweet.user_id  == f
+  				@following_tweets.push(tweet)
+  			end
+  		end
+  	end
+  end
+
+  def show_user
+    # Find the user
+    # TTS-SLIDE29
+    @user = User.find(params[:id])
+  end
+
+  def now_following
+      # TTS-SLIDE29
+      @user = User.find(params[:follow_id])
+
+      current_user.following.push(params[:follow_id].to_i)
+
+      current_user.save
+  end
+
+  def unfollow
+    # TTS-SLIDE38
+    @user = User.find(params[:unfollow_id])
+    
+    current_user.following.delete(params[:unfollow_id].to_i)
+
+    current_user.save
+  end
+
+  def js_practice
+
+
+  end
+
+end
